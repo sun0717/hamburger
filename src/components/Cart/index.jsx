d4e3d386d064e24a6f9c0fe4e92bc5951f660245
sun <743685264@qq.com>
2023-09-05 21:23:47 +0800

    完成购物车条

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -10,10 +10,10 @@ const Cart = () => {
     <div className={classes.Cart}>
       <div className={classes.Icon}>
         <img src={iconImg} />
-        <span className={classes.TotalAmount}>{ctx.totalAmount}</span>
+        {ctx.totalAmount === 0 ? null : <span className={classes.TotalAmount}>{ctx.totalAmount}</span>}
       </div>    
-      <p className={classes.Price}>{ctx.totalPrice}</p>
-      <button className={classes.Button}>去结算</button>
+      {ctx.totalAmount === 0 ? <p className={classes.NoMeal}>未选购商品</p> : <p className={classes.Price}>{ctx.totalPrice}</p>}
+      <button className={`${classes.Button} ${ctx.totalAmount === 0 ? classes.Disabled : ''}`}>去结算</button>
     </div>
   );
 };
